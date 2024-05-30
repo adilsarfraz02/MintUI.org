85f540f7aa45386921ab5a3f1ad7a7648fb0f264
Muhammad ADil <126439037+adilsarfraz02@users.noreply.github.com>
2024-05-30 17:54:04 +0500

    Create Links.tsx

---
+++ src/utils/Links.tsx
@@ -0,0 +1,20 @@
+export const NavLinks = [
+  {
+    title: "Get Started",
+    path: "/components/get-started",
+  },
+  {
+    title: "Components",
+    path: "/components",
+  },
+  {
+    title: "Blocks",
+    path: "/blocks",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+export const searchLinks = [{}];
